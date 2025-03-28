fbc4f2265e7a2400de9275ddf0278b2756434040
Ilya <ilyaduginov1997@gmail.com>
2025-03-28 08:50:25 +0300

    1.6 (feat): Add routing; Add lazy-components;

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+type AboutPropsType = {};
+
+const About: FC<AboutPropsType> = ({...props}) => {
+  return (
+    <div>About</div>
+  )
+};
+
+export default About;
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+type ProfilePropsType = {};
+
+const Profile: FC<ProfilePropsType> = ({...props}) => {
+  return (
+    <div>Profile</div>
+  )
+};
+
+export default Profile;
