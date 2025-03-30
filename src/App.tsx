d72f444fcf179d964665739ed1b68773ef7844c1
Ilya <ilyaduginov1997@gmail.com>
2025-03-30 22:50:26 +0300

    1.8 (feat): Add classNames function;

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,19 @@ import './styles/index.scss';
 import AboutLazy from "./pages/About/AboutLazy";
 import HomeLazy from "./pages/Home/HomeLazy";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App: FC = () => {
     const {toggleTheme, theme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle</button>
-            {/*<Link to={'/'}>Home</Link>*/}
             <Link to={'/'}>Home</Link>
             <Link to={'/about'}>About</Link>
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    {/*<Route path="/" element={<CounterComponent/>}/>*/}
                     <Route path="/" element={<HomeLazy/>}/>
                     <Route path="/about" element={<AboutLazy/>}/>
                 </Routes>
