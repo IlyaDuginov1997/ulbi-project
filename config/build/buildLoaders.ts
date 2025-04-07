841bf95cc39b484b248bfe34cff38ebe2530fdae
Ilya <ilyaduginov1997@gmail.com>
2025-04-07 08:58:32 +0300

    ulb-16-17 (feat): Add babel, add @babel/preset-env;

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -34,6 +34,21 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+
+            loader: "babel-loader",
+            options: {
+
+                presets: ['@babel/preset-env']
+
+            }
+
+        }
+    }
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff|woff2)$/i,
         use: [
@@ -47,6 +62,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         svgLoader,
         fileLoader,
         cssLoader,
+        babelLoader,
         typescriptLoader,
     ]
 }
